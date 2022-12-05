f9f157394913188b58bb7e5d0be42fdd39c94e77
Matthew Bronstein <mbronstein1@gmail.com>
2022-12-05 15:36:55 -0600

    finished individual product logic

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai'
+import { Product } from '../../components';
 import { client, urlFor } from '../../lib/client';
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
+    const [index, setIndex] = useState(0);
 
     return (
         <div>
             <div className="product-detail-container">
                 <div>
                     <div className="image-container">
-                        <img src={urlFor(image && image[0])} />
+                        <img src={urlFor(image && image[index])} className="product-detail-image"/>
                     </div>
-                    {/* <div className="small-images-container">
+                    <div className="small-images-container">
                         {image?.map((item, i) => (
                             <img 
                                 src={urlFor(item)}
-                                className=""
-                                onMouseEnter=""
+                                className={i === index ? "small-image selected-image" : "small-image"}
+                                onMouseEnter={() => setIndex(i)}
                             />
                         ))}
-                    </div> */}
+                    </div>
                 </div>
-                <div className="product-details-desc">
+                <div className="product-detail-desc">
                     <h1>{name}</h1>
                     <div className="reviews">
                         <div>
@@ -46,6 +48,21 @@ const ProductDetails = ({ product, products }) => {
                             <span className="num" onClick="">0</span>
                             <span className="plus" onClick=""><AiOutlinePlus /></span>
                         </p>
+                    </div>
+                    <div className="buttons">
+                        <button type="button" className="add-to-cart" onClick="">Add to Cart</button>
+                        <button type="button" className="buy-now" onClick="">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <div className="maylike-products-wrapper">
+                <h2>You may also like</h2>
+                <div className="marquee">
+                    <div className="maylike-products-container track">
+                        {products.map((item) => (
+                            <Product key={item._id}
+                            product={item}/>
+                        ))}
                     </div>
                 </div>
             </div>
