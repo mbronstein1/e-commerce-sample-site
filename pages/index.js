722758116abab29b1509aa38e7a2ce8d4756a516
Matthew Bronstein <mbronstein1@gmail.com>
2022-12-08 12:13:20 -0600

    implemented successful stripe payments in test mode

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import { Product, FooterBanner, HeroBanner} from '../components';
+import { Product, FooterBanner, HeroBanner } from '../components';
 import { client } from '../lib/client';
 
 const Home = ({ products, bannerData }) => {
   return (
     <div>
-      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
 
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {products?.map(
-          product => <Product 
-          key={product._id} product={product}/>)}
+        {products?.map(product =>
+          <Product key={product._id} product={product} />
+        )}
       </div>
 
-      <FooterBanner footerBanner={bannerData && bannerData[0]}/>
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </div>
   )
 }
